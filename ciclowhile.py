5ed758c44cfb13b4fcf71ce5fc190e8f895729cd
308 <308@academicos.cesde.edu.co>
2022-08-25 08:46:04 -0500

    NTJ04 - Se creo los bucles while

---
+++ ciclowhile.py
@@ -0,0 +1,23 @@
+'''numero = 5'''
+
+'''
+while numero < 10:
+    print("Estoy dentro de la cuerda")
+    numero = numero + 1
+
+print("Estoy fuera de la cuerda")
+'''
+
+opcion = 1
+
+print("***** Menu ******")
+print("*****************")
+
+print("1 : Sumar*")
+print("0 : Salir")
+
+while opcion != 0:
+    opcion = int(input("Digite una opción: "))
+    
+
+
